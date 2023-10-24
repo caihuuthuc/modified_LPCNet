4b0d1cc9e5015dfaf8b425641329a94e239496ab
Thuc Cai Huu <huuthuc997@gmail.com>
2023-10-24 20:39:54 +0700

    Update test_tt_gru.py

--- src/test_tt_gru.py
+++ src/test_tt_gru.py
@@ -12,6 +12,6 @@ rnn_layer(inp)
 
 tt_ranks2=[1, 8, 1]
 rnn_layer2 = TT_GRU(tt_input_shape=tt_input_shape, tt_output_shape=tt_output_shape, tt_ranks=tt_ranks2, debug=True)
-rnn_layer(inp)
+rnn_layer2(inp)
 
 #tt_input_shape=[16, 32], tt_output_shape=[8, 8], tt_ranks=[1, 3, 1],return_sequences=False, debug=True, dropout=.25, recurrent_dropout=.25, activation='tanh'
