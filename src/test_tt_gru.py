06052bcade1db3076c64f0e8104b11173929cf46
Thuc Cai Huu <huuthuc997@gmail.com>
2023-10-24 19:36:05 +0700

    Update test_tt_gru.py

--- src/test_tt_gru.py
+++ src/test_tt_gru.py
@@ -10,7 +10,7 @@ units = np.prod(np.array(tt_output_shape))
 
 rnn_layer = TT_GRU(tt_input_shape=[16, 32], tt_output_shape=[8, 8], tt_ranks=[1, 3, 1], debug=True)
 
-inp = np.random.rand(1, 64)
+inp = np.random.rand(1, 1, 16*32)
 rnn_layer(inp)
 
 
