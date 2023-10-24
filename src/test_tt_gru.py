cf8f3864bb9dfcaa4993c0df4430c3173fd11db8
Thuc Cai Huu <huuthuc997@gmail.com>
2023-10-24 16:27:13 +0700

    Update test_tt_gru.py

--- src/test_tt_gru.py
+++ src/test_tt_gru.py
@@ -1,6 +1,13 @@
 from TTLayer import *
 from TuykiTTRNN import *
 
-rnn_layer = TT_GRU(tt_input_shape=[16, 32], tt_output_shape=[8, 8], tt_ranks=[1, 3, 1],
-                   return_sequences=False, debug=True,
+tt_input_shape=[16, 32]
+
+tt_output_shape=[8, 8]
+tt_ranks=[1, 3, 1]
+units = np.prod(np.array(tt_output_shape))
+
+rnn_layer = TT_GRU(tt_input_shape=[16, 32], tt_output_shape=[8, 8], tt_ranks=[1, 3, 1],\
+                   units=units,\
+                   return_sequences=False, debug=True,\
                    dropout=.25, recurrent_dropout=.25, activation='tanh')
