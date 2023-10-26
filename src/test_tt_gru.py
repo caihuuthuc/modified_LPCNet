419d0c8b12e8e3d5f030c477d4cb375521fcd4c1
Thuc Cai Huu <huuthuc997@gmail.com>
2023-10-26 21:02:14 +0700

    Update test_tt_gru.py

--- src/test_tt_gru.py
+++ src/test_tt_gru.py
@@ -6,7 +6,7 @@ inp = np.random.rand(11, 1, 16*32)
 tt_input_shape=[16, 32]
 tt_output_shape=[4, 4]
 
-tt_ranks=[1, 4, 1]
+tt_ranks=[1, 8, 1]
 rnn_layer = TT_GRU(tt_input_shape=tt_input_shape, tt_output_shape=tt_output_shape, tt_ranks=tt_ranks, debug=True)
 out = rnn_layer(inp)
 
@@ -14,13 +14,13 @@ print("output shape: ", out.shape)
 
 print("------------------------------------------------------------------------------------------------")
 
-with open('/content/drive/MyDrive/recurrent_kernel_weight_of_grub.npy', 'rb') as f:
-        recurrent_kernel_weight = np.load(f)
+# with open('/content/drive/MyDrive/recurrent_kernel_weight_of_grub.npy', 'rb') as f:
+#         recurrent_kernel_weight = np.load(f)
   
-rnn2 = TT_GRU(tt_input_shape=tt_input_shape, tt_output_shape=tt_output_shape, tt_ranks=tt_ranks, 
-                  return_sequences=True, return_state=True, 
-                  recurrent_regularizer=initializers.Constant(recurrent_kernel_weight),
-                  name='tt_gru_b') 
+# rnn2 = TT_GRU(tt_input_shape=tt_input_shape, tt_output_shape=tt_output_shape, tt_ranks=tt_ranks, 
+#                   return_sequences=True, return_state=True, 
+#                   recurrent_regularizer=initializers.Constant(recurrent_kernel_weight),
+#                   name='tt_gru_b') 
 
 # tt_ranks2=[1, 8, 1]
 # rnn_layer2 = TT_GRU(tt_input_shape=tt_input_shape, tt_output_shape=tt_output_shape, tt_ranks=tt_ranks2, debug=True)
