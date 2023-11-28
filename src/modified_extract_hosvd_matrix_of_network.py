f057e7813e41dff1a48c527df5ab4418949f6569
Thuc Cai Huu <huuthuc997@gmail.com>
2023-11-29 06:02:17 +0700

    Update modified_extract_hosvd_matrix_of_network.py

--- src/modified_extract_hosvd_matrix_of_network.py
+++ src/modified_extract_hosvd_matrix_of_network.py
@@ -67,7 +67,7 @@ factor_2_weight = get_weights_by_name(model, factor_2_name).numpy()
 
 
 
-with open('/content/drive/MyDrive/size_2_3_2/kernel_weight_of_dualfc.npy', 'rb') as f:
+with open('/content/drive/MyDrive/kernel_weight_of_dualfc.npy', 'rb') as f:
     kernel_weight = np.load(f)
 
 rec = tenalg.multi_mode_dot(core_weight, [factor_0_weight, factor_1_weight, factor_2_weight])
