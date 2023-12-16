8200d8dd3e84deffb202c9766191b383a2b6f118
Thuc Cai Huu <huuthuc997@gmail.com>
2023-12-16 15:27:44 +0700

    Update modified_extract_hosvd_matrix_of_network.py

--- src/modified_extract_hosvd_matrix_of_network.py
+++ src/modified_extract_hosvd_matrix_of_network.py
@@ -63,7 +63,7 @@ model, enc, dec = lpcnet.new_lpcnet_model(use_gpu=False)
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
 
-model.load_weights('/content/drive/MyDrive/checkpoint_hosvd_lpcnet/hosvd_mdense_lpcnet20_384_10_G16_02.h5', by_name=True, skip_mismatch=True) 
+model.load_weights('/content/drive/MyDrive/checkpoint_hosvd_lpcnet/hosvd_mdense_lpcnet20_384_10_G16_02.h5') 
 
 # names = [weight.name for layer in model.layers for weight in layer.weights]
 # print(names)
