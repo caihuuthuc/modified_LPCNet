5d0a08c30a4aa04ba4e5a6bb217a4b1061ee0f5f
Thuc Cai Huu <huuthuc997@gmail.com>
2023-12-16 21:12:33 +0700

    Update modified_train_lpcnet.py

--- src/modified_train_lpcnet.py
+++ src/modified_train_lpcnet.py
@@ -18,7 +18,7 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.44
 
 set_session(tf.compat.v1.Session(config=config))
 
-nb_epochs = 5
+nb_epochs = 2
 
 # Try reducing batch_size if you run out of memory on your GPU
 batch_size = 128
