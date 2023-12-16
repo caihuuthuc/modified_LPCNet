ac6dfb43ed490e4c6ca39200d9756ace6cde54bb
Thuc Cai Huu <huuthuc997@gmail.com>
2023-12-16 16:12:10 +0700

    Update modified_train_lpcnet.py

--- src/modified_train_lpcnet.py
+++ src/modified_train_lpcnet.py
@@ -78,7 +78,7 @@ checkpoint = ModelCheckpoint('/content/drive/MyDrive/checkpoint_hosvd_lpcnet/hos
 
 #model.load_weights('lpcnet9b_384_10_G16_01.h5')
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate=0.0015,
+    initial_learning_rate=0.002,
     decay_steps=1000,
     decay_rate=0.9)
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
